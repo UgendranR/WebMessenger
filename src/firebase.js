cf87e8d32c22d9c648b73d4fb9fd23253876bca7
UgendranR <72851987+UgendranR@users.noreply.github.com>
2021-10-29 00:26:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_QXkcm2fkRJyoSB32UB6oPsZYSTBLMRc",
-  authDomain: "webchat-25764.firebaseapp.com",
-  databaseURL: "https://webchat-25764-default-rtdb.firebaseio.com",
-  projectId: "webchat-25764",
-  storageBucket: "webchat-25764.appspot.com",
-  messagingSenderId: "265589335677",
-  appId: "1:265589335677:web:78358b0f9317aa7dd57314",
-  measurementId: "G-L6CN4KHLF7"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "4",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp=firebase.initializeApp(firebaseConfig);
